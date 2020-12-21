69ce1c0b8c46cc558df134bcac98c51552edc22f
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-12-20 23:10:09 -0800

    hover makes colors

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 let size = 16;
+let index = 0;
+
+function getColor() {
+  return colors[index++ % colors.length];
+}
 
 function buildBoard() {
   let board = document.createElement('div');
@@ -8,6 +14,16 @@ function buildBoard() {
     for (let j = 0; j < size; j++) {
       let box = document.createElement('div');
       box.className = 'box';
+      box.style.backgroundColor = 'black';
+      box.addEventListener('mouseover', () => {
+        console.log(this);
+        let curColor = box.style.backgroundColor;
+        let nextColor = getColor();
+        while (nextColor === curColor) {
+          nextColor = getColor();
+        }
+        box.style.backgroundColor = nextColor;
+      });
       row.appendChild(box);
     }
     board.appendChild(row);
