bd15696116d996da0419974f1529303e02d5cc57
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-12-20 23:17:39 -0800

    Fin

--- script.js
+++ script.js
@@ -2,6 +2,20 @@ const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 let size = 16;
 let index = 0;
 
+document.querySelector('#reset').addEventListener('click', resetBoard);
+
+function resetBoard() {
+  let boardSize = prompt('How big will the board be? Choose between 1 and 30');
+  while (!Number.isInteger(Number(boardSize)) || Number(boardSize) > 31) {
+    boardSize = prompt('How big will the board be?');
+  }
+  size = boardSize;
+  document
+    .querySelector('#board')
+    .removeChild(document.querySelector('#board').firstChild);
+  buildBoard();
+}
+
 function getColor() {
   return colors[index++ % colors.length];
 }
